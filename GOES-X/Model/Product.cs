bc47c7db40d21ac62472b0635cfa201d58ce0d9b
NoOverflow <julien.prost@epitech.eu>
2021-10-23 19:06:01 +0200

    Add comments for Product class

--- GOES-X/Model/Product.cs
+++ GOES-X/Model/Product.cs
@@ -25,10 +25,29 @@ namespace GOES_X.Model
     [Serializable]
     public class Product
     {
+        /// <summary>
+        /// The GOES Product index based on GOES-R series satellite specifications
+        /// </summary>
+        public GoesProduct ProductIndex { get; set; }
 
+        /// <summary>
+        /// User-friendly product name
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// The current opacity for this product, unused if not currently displayed by the user
+        /// </summary>
         public double Opacity { get; set; }
-        public float BestSpatialResolution { get; set;}
-        public float CentralWaveLength { get; set;}
+
+        /// <summary>
+        /// Best spatial resolution for this product, expressed with a factor (0.5x, 2x...)
+        /// </summary>
+        public float BestSpatialResolution { get; set; }
+
+        /// <summary>
+        /// The central wavelength of this product (in nm)
+        /// </summary>
+        public float CentralWaveLength { get; set; }
     }
 }
