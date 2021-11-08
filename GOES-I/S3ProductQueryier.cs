1a3722007d5cc5ae532133aa7a2eec4fab9ca017
NoOverflow <julien.prost@epitech.eu>
2021-11-08 23:22:12 +0100

    Wip GOES-I

---
+++ GOES-I/S3ProductQueryier.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace GOES_I
+{
+    public class S3ProductQueryier
+    {
+        public AmazonS3Client Client { get; set; }
+
+        private const string GOES_16_BUCKET = "noaa-goes16";
+
+        public S3ProductQueryier(AmazonS3Client Client)
+        {
+            this.Client = Client;
+        }
+
+        public async Task ListRawProducts()
+        {
+            ListObjectsRequest listRequest = new ListObjectsRequest
+            {
+                BucketName = GOES_16_BUCKET,                
+            };
+            ListObjectsResponse listResponse;
+
+            do
+            {
+                listResponse = await Client.ListObjectsAsync(listRequest);
+                foreach (S3Object obj in listResponse.S3Objects)
+                {
+                    Console.WriteLine("Object - " + obj.Key);
+                    Console.WriteLine(" Size - " + obj.Size);
+                    Console.WriteLine(" LastModified - " + obj.LastModified);
+                    Console.WriteLine(" Storage class - " + obj.StorageClass);
+                }
+                listRequest.Marker = listResponse.NextMarker;
+            } while (listResponse.IsTruncated);
+        }
+    }
+}
---
+++ GOES-ITest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GOES_I;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace GOES_ITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AmazonS3Client client = new AmazonS3Client(new Amazon.Runtime.AnonymousAWSCredentials(), Amazon.RegionEndpoint.USEast1);
+            S3ProductQueryier pqueryier = new S3ProductQueryier(client);
+
+            pqueryier.ListRawProducts().Wait();
+        }
+    }
+}
