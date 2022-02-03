9adc683907412d0a34239a63599dd721a29da239
NoOverflow <julien.prost@epitech.eu>
2022-02-03 14:40:04 +0100

    Add temporary bands selection

---
+++ GOES-X/Services/UserPreferencesService.cs
@@ -0,0 +1,23 @@
+﻿namespace GOES_X.Services
+{
+    public class UserPreferencesService
+    {
+        private int _bandSelected = 1;
+        public int BandSelected
+        {
+            get
+            {
+                return _bandSelected;
+            }
+
+            set
+            {
+                _bandSelected = value;
+                OnUserPreferencesUpdate?.Invoke();
+            }
+        }
+
+        public delegate void OnUserPreferencesUpdateDelegate();
+        public event OnUserPreferencesUpdateDelegate? OnUserPreferencesUpdate;
+    }
+}
